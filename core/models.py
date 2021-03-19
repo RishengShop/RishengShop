78d8579e4bccbf2a6fbd45637f1e92930a7d2e33
unknown <zhaoran.Wang@hotmail.com>
2021-03-19 18:19:17 +0100

    update

--- core/models.py
+++ core/models.py
@@ -6,17 +6,17 @@ from django.shortcuts import reverse
 from django_countries.fields import CountryField
 
 
-CATEGORY_CHOICES = (
-    ('A', '方便速食'),
-    ('B', '火锅料理'),
-    ('C', '生鲜蛋面'),
-    ('D', '休闲零食'),
-    ('E', '五谷干货'),
-    ('F', '鲜香调味'),
-    ('G', '时令果蔬'),
-    ('H', '冰冻食品'),
-    ('I', '其他')
-)
+# CATEGORY_CHOICES = (
+#     ('A', '方便速食'),
+#     ('B', '火锅料理'),
+#     ('C', '生鲜蛋面'),
+#     ('D', '休闲零食'),
+#     ('E', '五谷干货'),
+#     ('F', '鲜香调味'),
+#     ('G', '时令果蔬'),
+#     ('H', '冰冻食品'),
+#     ('I', '其他')
+# )
 
 
 class Category(models.Model):
