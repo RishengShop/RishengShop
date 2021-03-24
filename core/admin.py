1f5aae9e94fb7898584411a15d2a84ff0be90f6b
Lipschitz <zhaoran.Wang@hotmail.com>
2021-03-24 23:21:34 +0100

    admin_page

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Item, OrderItem, Order, Payment, Coupon, Refund, Address, UserProfile,Category
+from .models import Item, OrderItem, Order, Payment, Coupon, Refund, Address, UserProfile, Category
 
+from django.contrib.auth.models import User
 
 def make_refund_accepted(modeladmin, request, queryset):
     queryset.update(refund_requested=False, refund_granted=True)
@@ -11,6 +12,32 @@ def make_being_delivered(modeladmin, request, queryset):
 
 make_refund_accepted.short_description = 'Update orders to refund granted'
 make_being_delivered.short_description = 'Update orders to being delivered'
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['get_title',
+                    'get_price',
+                    'get_discount_price',
+                    'get_category_name'
+                    ]
+    list_filter = ('category_id',)
+    search_fields = ['title',]
+
+    def get_title(self, obj):
+        return obj.title
+    get_title.short_description = u'商品'
+
+    def get_price(self, obj):
+        return obj.price
+    get_price.short_description = u'价格'
+
+    def get_discount_price(self, obj):
+        return obj.discount_price
+    get_discount_price.short_description = u'折扣价'
+
+    def get_category_name(self, obj):
+
+        return Category.objects.filter(id = obj.category_id)[0]
+    get_category_name.short_description = u'类别'
 
 
 class OrderAdmin(admin.ModelAdmin):
@@ -37,28 +64,79 @@ class OrderAdmin(admin.ModelAdmin):
                    'received',
                    'refund_requested',
                    'refund_granted']
+
     search_fields = [
         'user__username',
         'ref_code'
     ]
     actions = [make_refund_accepted, make_being_delivered]
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['get_category_name',
+                    'get_category_count'
+                    ]
+
+
+    def get_category_name(self, obj):
+        return obj.name
+
+    get_category_name.short_description = u'类别'
+
+    def get_category_count(self, obj):
+        return len(Item.objects.filter(category_id = obj.id))
+
+    get_category_count.short_description = u'数量'
 
 class OrderItemAdmin(admin.ModelAdmin):
 
     list_display = [
-        'user',
-        'item',
-        'quantity',
+        'get_user_id',
+        'get_user_name',
+        'get_item_name',
+        'get_item_price',
+        'get_item_discount_price',
+        'get_quantity'
     ]
 
-    list_display_links = [
-        'user',
-        'item',
+    list_filter = [
+        ''
+        'item_id__category',
     ]
 
-    search_fields = ['user__username', 'item']
 
+
+    search_fields = ['item__title','user__username', 'user__id']
+
+    def get_user_id(self, obj):
+        return User.objects.filter(id = obj.user_id)[0].id
+
+    get_user_id.short_description = '账号'
+
+    def get_user_name(self, obj):
+        return User.objects.filter(id = obj.user_id)[0].username
+
+    get_user_name.short_description = u'姓名'
+
+    def get_item_price(self, obj):
+
+        return Item.objects.filter(id = obj.item_id)[0].price
+
+    get_item_price.short_description = u'价格'
+
+    def get_item_name(self, obj):
+        return obj.item
+
+    get_item_name.short_description = u'商品'
+
+    def get_item_discount_price(self, obj):
+        return Item.objects.filter(id = obj.item_id)[0].discount_price
+
+    get_item_discount_price.short_description = u'折扣价'
+
+    def get_quantity(self, obj):
+        return obj.quantity
+
+    get_quantity.short_description = u'数量'
 
 
 class AddressAdmin(admin.ModelAdmin):
@@ -75,7 +153,7 @@ class AddressAdmin(admin.ModelAdmin):
     search_fields = ['user', 'street_address', 'apartment_address', 'zip']
 
 
-admin.site.register(Item)
+admin.site.register(Item, ItemAdmin)
 admin.site.register(OrderItem, OrderItemAdmin)
 admin.site.register(Order, OrderAdmin)
 # admin.site.register(Payment)
@@ -83,4 +161,4 @@ admin.site.register(Order, OrderAdmin)
 # admin.site.register(Refund)
 admin.site.register(Address, AddressAdmin)
 # admin.site.register(UserProfile)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
