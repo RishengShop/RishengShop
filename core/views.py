3d1416107c83039704b4ae24077d316ba903a2df
Eric Coleman <39775171+NiiColeman@users.noreply.github.com>
2021-05-11 14:12:17 +0200

    items reverse

--- core/views.py
+++ core/views.py
@@ -24,7 +24,7 @@ def create_ref_code():
 
 def products(request):
     context = {
-        'items': Item.objects.all()
+        'items': Item.objects.all().reverse()
     }
     return render(request, "products.html", context)
 
